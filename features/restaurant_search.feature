499df3399f4ef4635dcc8d39d7cf2bfaa678aeef
Pradeep Sethi <psethi@gmail.com>
2013-03-04 18:34:35 +0530

    fixed breaking steps, added respec commands

--- features/restaurant_search.feature
+++ features/restaurant_search.feature
@@ -9,5 +9,11 @@ Then I should see atleast 3 results with "chinese" as Primary cuisine
 	And Page should list "The China Kitchen"
 	And I should not see results with "south indian" as Primary cuisine
 	
-	
+Scenario: Searching for south Indian cuisine
+Given I am on Restaurant Home Page
+When I enter "south indian" in keyword search box
+	And I click Find
+Then I should see atleast 3 results with "south indian" as Primary cuisine
+	And Page should list "Swagath Bar and Restaurant"
+	And I should not see results with "mughlai" as Primary cuisine	
 	
--- features/step_definitions/restaurant_search_steps.rb
+++ features/step_definitions/restaurant_search_steps.rb
@@ -13,20 +13,29 @@ end
 Then /^I should see atleast (\d+) results with "(.*?)" as Primary cuisine$/ do |num_results, cuisine_name|
 
   results = page.all(:css, "div.desarea li.desstate", :visible => true)
+  # debugger
   cuisine_match_count = 0
   results.each  { | result |
   cuisine_match_count += 1 if (result.text.match("Cuisine : #{cuisine_name}"))
     }
   
-  cuisine_match_count > num_results.to_i
+  cuisine_match_count.should be >= num_results.to_i
   
 end
 
 Then /^Page should list "(.*?)"$/ do |restaurant_name|
-    page.has_content?(restaurant_name)
-    page.save_screenshot('screenshot.png')
+    page.should have_content(restaurant_name)
+    page.save_screenshot('screenshot_new.png')
+    # save_and_open_page
 end
 
-Then /^I should not see results with "(.*?)" as Primary cuisine$/ do |cuisine|
-  ! page.has_content?(cuisine)
+Then /^I should not see results with "(.*?)" as Primary cuisine$/ do |cuisine_name|
+    results = page.all(:css, "div.desarea li.desstate", :visible => true)
+    # debugger
+    cuisine_match_count = 0
+    results.each  { | result |
+    cuisine_match_count += 1 if (result.text.match("Cuisine : #{cuisine_name}"))
+      }
+  
+    cuisine_match_count.should == 0
 end
